2bbb02f1a0b59618aa70cc003415819b917d488e
thien.nguyen <nhatthien185@gmail.com>
2023-12-02 15:31:19 +0700

    prettier config

--- 2023/day-2/index.ts
+++ 2023/day-2/index.ts
@@ -1,72 +1,79 @@
-import { readFile } from "../../utils";
+import { readFile } from "../../utils"
 
-const input = readFile('2023', 'day-2', 'input.txt')
-const games = input.split('\n')
+const input = readFile("2023", "day-2", "input.txt")
+const games = input.split("\n")
 const limit = {
-    red: 12,
-    green: 13,
-    blue: 14
+  red: 12,
+  green: 13,
+  blue: 14
 }
 
 type Turn = Record<string, number>
 
 interface Game {
-    ID: number
-    turns: Turn[]
+  ID: number
+  turns: Turn[]
 }
 
 const isTurnPossible = (turn: Turn) => {
-    return Object.entries(turn).every(([cube, quantity]) => quantity <= limit[cube])
+  return Object.entries(turn).every(
+    ([cube, quantity]) => quantity <= limit[cube]
+  )
 }
 
-const isGamePossible = (turns: Game['turns']) => {
-    return turns.every(isTurnPossible)
+const isGamePossible = (turns: Game["turns"]) => {
+  return turns.every(isTurnPossible)
 }
 
 const gameStringToGame = (game: string): Game => {
-    const [gameIDString, turnsString] = game.split(': ')
-    const gameID = Number(gameIDString.split(' ')[1])
-    const turns: Turn[] = turnsString.split('; ').map(turnString => {
-        const cubes = turnString.split(', ')
-        return cubes.reduce<Turn>((result, cubeString) => {
-            const [quantity, cube] = cubeString.split(' ')
-            result[cube] = parseInt(quantity)
-            return result
-        }, {})
-    })
-    return {
-        ID: gameID,
-        turns
-    }
+  const [gameIDString, turnsString] = game.split(": ")
+  const gameID = Number(gameIDString.split(" ")[1])
+  const turns: Turn[] = turnsString.split("; ").map((turnString) => {
+    const cubes = turnString.split(", ")
+    return cubes.reduce<Turn>((result, cubeString) => {
+      const [quantity, cube] = cubeString.split(" ")
+      result[cube] = parseInt(quantity)
+      return result
+    }, {})
+  })
+  return {
+    ID: gameID,
+    turns
+  }
 }
 
 const firstResult = games.reduce((total, gameString) => {
-    const game = gameStringToGame(gameString)
-    if (isGamePossible(game.turns)) {
-        return total + game.ID
-    }
-    return total
+  const game = gameStringToGame(gameString)
+  if (isGamePossible(game.turns)) {
+    return total + game.ID
+  }
+  return total
 }, 0)
 
-console.log('firstResult: ', firstResult)
+console.log("firstResult: ", firstResult)
 
 // Part 2
 const getPowerOfGame = (game: Game): number => {
-    return Object.values(game.turns.reduce((power, turn) => {
+  return Object.values(
+    game.turns.reduce(
+      (power, turn) => {
         Object.entries(turn).forEach(([cube, quantity]) => {
-            if (quantity > power[cube]) {
-                power[cube] = quantity
-            }
+          if (quantity > power[cube]) {
+            power[cube] = quantity
+          }
         })
         return power
-    }, { red: 1, green: 1, blue: 1})).reduce((total, quantity) => {
-        return total * quantity
-    }, 1)
+      },
+      { red: 1, green: 1, blue: 1 }
+    )
+  ).reduce((total, quantity) => {
+    return total * quantity
+  }, 1)
 }
 
 const secondResult = games.reduce((total, gameString) => {
-    const game = gameStringToGame(gameString)
-    return total + getPowerOfGame(game)
+  const game = gameStringToGame(gameString)
+  return total + getPowerOfGame(game)
 }, 0)
 
-console.log('secondResult: ', secondResult)
+console.log("secondResult: ", secondResult)
--- utils.ts
+++ utils.ts
@@ -1,14 +1,17 @@
-import {readFileSync} from "fs";
-import * as process from "process";
-import * as path from 'path';
+import { readFileSync } from "fs"
+import * as process from "process"
+import * as path from "path"
 
 const adventOfCodePath = {
-    '2023': {
-        'day-1': '/2023/day-1',
-        'day-2': '/2023/day-2'
-    }
+  "2023": {
+    "day-1": "/2023/day-1",
+    "day-2": "/2023/day-2"
+  }
 }
 
 export const readFile = (year: string, day: string, inputFileName: string) => {
-    return readFileSync(path.join(process.cwd(), adventOfCodePath[year][day], inputFileName), { encoding: 'utf-8' })
+  return readFileSync(
+    path.join(process.cwd(), adventOfCodePath[year][day], inputFileName),
+    { encoding: "utf-8" }
+  )
 }
