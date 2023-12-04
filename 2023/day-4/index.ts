9ef6cc938b0f9271742037cef0fc7274f28578f4
thien.nguyen <nhatthien185@gmail.com>
2023-12-04 15:46:39 +0700

    2023 day-4 part2

--- 2023/day-4/index.ts
+++ 2023/day-4/index.ts
@@ -20,24 +20,53 @@ function cardStringToCard(text: string): Card {
   }
 }
 
-function getPointFromCard(card: Card) {
-  const count =
-    card.winningNumbers.reduce((matchCount, number) => {
-      if (card.numbers.includes(number)) {
-        matchCount += 1
-      }
-      return matchCount
-    }, 0) - 1
+function getPointFromMatchCount(count: number) {
+  if (count - 1 < 0) return 0
+  return Math.pow(2, count - 1)
+}
 
-  if (count < 0) return 0
-  return Math.pow(2, count)
+function getMatchCountFromCard(card: Card) {
+  return card.winningNumbers.reduce((matchCount, number) => {
+    if (card.numbers.includes(number)) {
+      matchCount += 1
+    }
+    return matchCount
+  }, 0)
 }
 
 function getResultPart1() {
   return cards.reduce((total, cardString) => {
     const card = cardStringToCard(cardString)
-    return total + getPointFromCard(card)
+    return total + getPointFromMatchCount(getMatchCountFromCard(card))
   }, 0)
 }
 
 console.log("firstResult: ", getResultPart1())
+
+// Part 2
+const cardInstances = new Map<number, number>(
+  Array.from({ length: cards.length }).map((_, index) => [index, 1])
+)
+
+function getResultPart2() {
+  for (let cardIndex = 0; cardIndex < cards.length; cardIndex++) {
+    const card = cardStringToCard(cards[cardIndex])
+    const matchCount = getMatchCountFromCard(card)
+    const prizeInstances = cardInstances.get(cardIndex)
+
+    for (
+      let nextCardIndex = cardIndex + 1;
+      nextCardIndex < cardIndex + matchCount + 1;
+      nextCardIndex++
+    ) {
+      const currentCardInstances = cardInstances.get(nextCardIndex)
+      cardInstances.set(nextCardIndex, currentCardInstances + prizeInstances)
+    }
+  }
+
+  return Array.from(cardInstances.values()).reduce((total, instanceNumber) => {
+    return total + instanceNumber
+  }, 0)
+}
+
+console.log("secondResult: ", getResultPart2())
