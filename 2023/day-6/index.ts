04b6484200482ddf536062073bf00ce9e7e630c1
thien.nguyen <nhatthien185@gmail.com>
2023-12-06 16:10:54 +0700

    2023 day-6 part1

---
+++ 2023/day-6/index.ts
@@ -0,0 +1,49 @@
+import { readFile } from "../../utils"
+
+interface Race {
+  time: number
+  distance: number
+}
+
+const input = readFile("2023", "day-6", "input.txt")
+const [timesString, distancesString] = input.split("\n")
+const times = timesString
+  .split(": ")[1]
+  .split(" ")
+  .filter((num) => num)
+  .map(Number)
+const distances = distancesString
+  .split(": ")[1]
+  .split(" ")
+  .filter((num) => num)
+  .map(Number)
+const races: Race[] = []
+
+for (let i = 0; i < times.length; i++) {
+  races.push({ time: times[i], distance: distances[i] })
+}
+
+function getDistance(speed: number, time: number) {
+  return (time - speed) * speed
+}
+
+function getPossibleSpeedQuantityFromRace(race: Race) {
+  const maxSpeed = race.time - 1
+  let count = 0
+
+  for (let speed = 1; speed <= maxSpeed; speed++) {
+    const distance = getDistance(speed, race.time)
+    const lastDistance = getDistance(speed - 1, race.time)
+    if (distance < lastDistance) return count * 2 - 1
+    if (distance === lastDistance) return count * 2
+    if (distance > race.distance) count++
+  }
+}
+
+function getResultPart1() {
+  return races.reduce((acc, race) => {
+    return acc * getPossibleSpeedQuantityFromRace(race)
+  }, 1)
+}
+
+console.log("firstResult: ", getResultPart1())
--- utils.ts
+++ utils.ts
@@ -8,13 +8,11 @@ const adventOfCodePath = {
     "day-2": "/2023/day-2",
     "day-3": "/2023/day-3",
     "day-4": "/2023/day-4",
-    "day-5": "/2023/day-5"
+    "day-5": "/2023/day-5",
+    "day-6": "/2023/day-6"
   }
 }
 
 export const readFile = (year: string, day: string, inputFileName: string) => {
-  return readFileSync(
-    path.join(process.cwd(), adventOfCodePath[year][day], inputFileName),
-    { encoding: "utf-8" }
-  )
+  return readFileSync(path.join(process.cwd(), adventOfCodePath[year][day], inputFileName), { encoding: "utf-8" })
 }
