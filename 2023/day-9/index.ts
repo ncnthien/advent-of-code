ad59f6490cb0c43130d7f77bd9389652a4ad2472
thien.nguyen <nhatthien185@gmail.com>
2023-12-10 14:06:29 +0700

    2023 day-9 part2

--- 2023/day-9/index.ts
+++ 2023/day-9/index.ts
@@ -26,5 +26,27 @@ function getPart1Result() {
   }, 0)
 }
 
-console.log('firstResult:', getPart1Result())
+console.log('firstResult: ', getPart1Result())
 
+// Part 2
+
+function getFirstValueOfHistory(history: number[]) {
+  const differenceHistory: number[] = []
+
+  for (let i = 1; i < history.length; i++) {
+    differenceHistory[i - 1] = history[i] - history[i - 1]
+  }
+
+  if (isLastDifferenceHistory(differenceHistory)) {
+    return history.at(0)
+  }
+  return history.at(0) - getFirstValueOfHistory(differenceHistory)
+}
+
+function getPart2Result() {
+  return histories.reduce((totalValue: number, history: number[]) => {
+    return totalValue + getFirstValueOfHistory(history)
+  }, 0)
+}
+
+console.log('secondResult: ', getPart2Result())
